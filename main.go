e20b7f76f6a5c66d8342878e203c09ad57c2f86f
zhoupengyu <zhoupengyu0926@163.com>
2021-08-04 14:32:09 +0800

    增加gin

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("good")
-	time.Sleep(time.Minute * 5)
+	// 1.创建路由
+	r := gin.Default()
+	// 2.绑定路由规则，执行的函数
+	// gin.Context，封装了request和response
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "你好!这是gin")
+	})
+	// 3.监听端口，默认在8080
+	// Run("里面不指定端口号默认为8080")
+	if err := r.Run(":8600").Error(); err != "" {
+		panic("发生错误")
+	}
 }
